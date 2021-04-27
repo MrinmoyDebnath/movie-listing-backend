857395545709c84cf95ba31a5be5542a3cb5dbf2
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-27 13:22:58 +0530

    added user route

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const { SERVER_PORT } = require('./config');
 const actors = require('./routes/actors');
 const producers = require('./routes/producers');
 const movies = require('./routes/movies');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -14,7 +16,9 @@ app.use(bodyParser.json());
 app.use('/actors', actors);
 app.use('/producers', producers);
 app.use('/movies', movies);
+app.use('/user', users)
 
-app.listen(3000, ()=>{
-    console.log('listening on http://localhost:3000');
+const port = SERVER_PORT || 3001;
+app.listen(port, ()=>{
+    console.log(`listening on http://localhost:${port}`);
 })
