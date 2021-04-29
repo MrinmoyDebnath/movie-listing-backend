1a47435a9b1d19cd2bccace18455cedee56f52c2
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-29 11:56:24 +0530

    replaced Client usage with Pool usage

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
-const { Client } = require('pg');
-const { USER, HOST, DATABASE, PASSWORD, PORT } = require('./config');
+const { Pool } = require('pg');
 
-const db = new Client({
+const USER = process.env.USER;
+const HOST = process.env.HOST;
+const DATABASE = process.env.DATABASE;
+const PORT = process.env.PORT;
+const PASSWORD = process.env.PASSWORD;
+
+const db = new Pool({
     user: USER,
     host: HOST,
     database: DATABASE,
     password: PASSWORD,
     port: PORT,
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0
 })
 db.connect();
 
