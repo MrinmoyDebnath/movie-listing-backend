a9ebaf7d1240b24f6bfa23b7a657b359404b1b6d
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-30 16:44:09 +0530

    refactored the use of auth middleware

--- app.js
+++ app.js
@@ -4,29 +4,30 @@ const cors = require('cors');
 require('dotenv').config()
 
 const SERVER_PORT = process.env.SERVER_PORT;
+const HOST = process.env.HOST;
 
 const actors = require('./routes/actors');
 const producers = require('./routes/producers');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
-const auth = require('./auth');
+const {authenticate} = require('./auth');
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.use('/actors', auth, actors);
-app.use('/producers', auth, producers);
-app.use('/movies', auth, movies);
+app.use('/actors', authenticate, actors);
+app.use('/producers', authenticate, producers);
+app.use('/movies', authenticate, movies);
 app.use('/user', users)
-app.use((req, res, next)=>{
+app.use((req, res)=>{
     res.status(400).send({
-        error: "Not Found"
+        error: "Page Not Found"
     })
 })
 
 const port = SERVER_PORT || 3001;
 app.listen(port, ()=>{
-    console.log(`listening on http://localhost:${port}`);
+    console.log(`listening on http://${HOST}:${port}`);
 })
